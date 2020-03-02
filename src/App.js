3a2cac59adc931033243231f09a5fa0c13e35a4c
Shujaat Azim <shujaat.azim@gmail.com>
2020-03-02 17:55:50 -0500

    Added React Bootstrap and Router packages, tested bootstrap by making a button. Works. Fresh scaffold to start development.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div>
-      Sup
+      <Button color="primary">Better Dex</Button>
     </div>
   )
 }
