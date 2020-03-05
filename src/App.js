cf9cbd57f636fc895b3b139e79fe0de02a5bc356
Shujaat Azim <shujaat.azim@gmail.com>
2020-03-05 17:59:03 -0500

    Built out functions for randomizing six pokemon on the front page. Not totally done yet, but will work on it. Changed a little styling on homepage too. Added ability to click a button to mark a pokemon as complete if the user has decided that they have already updated the pokedex entry to their liking.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,49 @@ import React, { useState, useEffect } from 'react'
 import PokemonCards from './Components/PokemonCards'
 import { Button, Jumbotron, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
 
-function App() {
+const App = () => {
 
   const [allPokemon, setAllPokemon] = useState([])
-  const [allEntries, setAllEntries] = useState([])
+  const [randoms, setRandoms] = useState([])
 
   useEffect(() => {
+    const randomSix = () => {
+      let i = 6
+      let randomInts = []
+      while (i > 0) {
+        let num = getRandomInt()
+        if (randomInts.includes(num)) {
+          num = getRandomInt()
+        } else {
+          randomInts.push(num)
+          i--
+        }
+      }
+      // let finalSix = randomInts.map(num => allPokemon.filter(pokemon => pokemon.id === num))
+      // setDisplayed(finalSix)
+      // console.log(displayed)
+    }
+
+    const filtration = () => {
+      let finalSix = []
+      for (let i = 0; i > randoms.length; i++) {
+        let found = allPokemon.find(pokemon => pokemon.id === randoms[i])
+        finalSix.push(found)
+      }
+      console.log(finalSix)
+    }
+    
     fetch('http://localhost:3000/pokemons')
     .then(resp => resp.json())
     .then(data => setAllPokemon(data))
-    
-    fetch('http://localhost:3000/pokedexes')
-    .then(resp => resp.json())
-    .then(data => setAllEntries(data))
-
+    .then(() => randomSix())
+    .then(() => filtration())
   }, [])
+
+  const getRandomInt = () => {
+    return Math.floor(Math.random() * Math.floor(6) + 1)
+  }
+  
 
   return (
     <div>
@@ -26,11 +54,11 @@ function App() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/pokemon">Pokemon</Nav.Link>
+            <Nav.Link href="/pokemon">All Pokemon</Nav.Link>
             <Nav.Link href="">Random</Nav.Link>
             <NavDropdown title="Placeholder" id="basic-nav-dropdown">
-              <NavDropdown.Item href="">Placeholder 1</NavDropdown.Item>
-              <NavDropdown.Item href="">Placeholder 2</NavDropdown.Item>
+              <NavDropdown.Item href="">Kanto # 001-151</NavDropdown.Item>
+              <NavDropdown.Item href="">Johto # 152-251</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form inline>
@@ -42,20 +70,16 @@ function App() {
 
       <Jumbotron>
         <h1>Welcome to Better Pokedex Entries!</h1>
-        <p>
-          We know the Pokedex in the games can be a little... lackluster. Practice making some!
-        </p>
+        <p>We know the Pokedex in the games can be a little... lackluster. Practice making some!</p>
         <hr className="my-2" />
-        <p>
-          Start by choosing a pokemon!
-        </p>
-        <p>
-          <Button color="primary">Pokemon</Button>
-        </p>
+        <p>Start by choosing a pokemon!</p>
+        <p><Button color="primary">Pokemon</Button></p>
       </Jumbotron>
 
+
+
       {allPokemon.map(pokemon => {
-        return <PokemonCards pokemon={pokemon} />
+        return <PokemonCards key={pokemon.id} pokemon={pokemon} />
       })}
 
     </div>
--- src/Components/PokemonCards.js
+++ src/Components/PokemonCards.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Button } from 'react-bootstrap'
 
 const PokemonCards = props => {
+
+  const [complete, setComplete] = useState(false)
   
   return (
-    <Card style={{ width: '18rem', height: "100%", display: 'inline-block' }}>
+    <Card style={{ width: '18rem', height: "100%", display: 'inline-block', margin: "1rem" }}>
+      <Card.Header>National Dex #{props.pokemon.id}</Card.Header>
       <Card.Img style={{ width: "100%", height: "15vw", objectFit: "contain" }} 
         variant="top" src={`https://img.pokemondb.net/artwork/${props.pokemon.name.toLowerCase()}.jpg`} />
       <Card.Body>
-        <Card.Title style={{ align: "center" }}>{props.pokemon.name}</Card.Title>
+        <Card.Title>{props.pokemon.name}</Card.Title>
+        <hr />
         <Card.Text>
-          <p>
-            <Button style={{ marginRight: "0.5rem" }}>Edit Pokemon</Button>
-            <Button style={{ MarginLeft: "0.5rem" }}>Pokedex</Button>
-          </p>
+          <Button style={{ marginRight: "0.5rem" }}>Info Page</Button>
+          <Button onClick={() => setComplete(!complete)} variant={complete ? "success" : "outline-secondary"} style={{ marginLeft: "0.5rem" }}>
+            { complete? "Complete!" : "Mark Complete" }</Button>
         </Card.Text>
       </Card.Body>
     </Card>
