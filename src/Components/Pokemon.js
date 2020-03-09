7ef12ae834beda1e03061c554c142ef85e7a5b89
Shujaat Azim <shujaat.azim@gmail.com>
2020-03-08 23:59:15 -0400

    Trying new random.

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -19,6 +19,7 @@ const Pokemon = () => {
     let num = Math.floor(Math.random() * allPokemon.length)
     let rando = pokes.filter(pokemon => pokemon.id === num)
     setRandomPokemon(rando[0])
+    // window.location = '/pokemon/randomPokemon.name.toLowerCase()'
   }
 
 // this is for generating random pokes:
@@ -27,23 +28,23 @@ const Pokemon = () => {
   return (
     <div>
       <Navbar>
-      <Navbar.Brand href="/">Better Pokedex</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/pokemon">All Pokemon</Nav.Link>
-            <Nav.Link href="" onClick={() => getRandomPoke()}>Random</Nav.Link>
-            <NavDropdown title="Placeholder" id="basic-nav-dropdown">
-              <NavDropdown.Item href="">Kanto # 001-151</NavDropdown.Item>
-              <NavDropdown.Item href="">Johto # 152-251</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-primary">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+        <Navbar.Brand href="/">Better Pokedex</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/pokemon">All Pokemon</Nav.Link>
+              <Nav.Link href="" onClick={() => getRandomPoke()}>Random</Nav.Link>
+              <NavDropdown title="Placeholder" id="basic-nav-dropdown">
+                <NavDropdown.Item href="">Kanto # 001-151</NavDropdown.Item>
+                <NavDropdown.Item href="">Johto # 152-251</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-primary">Search</Button>
+            </Form>
+          </Navbar.Collapse>
       </Navbar>
     {allPokemon.map(pokemon => {
       return <PokemonCards key={pokemon.id} pokemon={pokemon} />
