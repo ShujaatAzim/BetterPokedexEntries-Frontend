ff5b9f98ab5450bf833cbe1346b93ae013712118
Shujaat Azim <shujaat.azim@gmail.com>
2020-04-08 20:46:35 -0400

    Added filter for random pokemon such that they show up in ascending order, but only works 5/6 times for some reason.

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ const App = () => {
     let randomIDs = []
     let newPokes = []
     while (randomIDs.length < 6) {
-      let num = Math.floor(Math.random() * 12) + 1
+      let num = Math.floor(Math.random() * 15) + 1
       if (!randomIDs.includes(num)) {
         randomIDs.push(num)
+        randomIDs.sort((a, b) => a - b)
       }
     }
     
@@ -29,34 +30,32 @@ const App = () => {
 
   return (
     <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Better Pokedex</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/pokemon">All Pokemon</Nav.Link>
+            <Nav.Link href="">Random</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-primary">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
       
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">Better Pokedex</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/pokemon">All Pokemon</Nav.Link>
-              <Nav.Link href="">Random</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-primary">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
+      <Jumbotron>
+        <h1>Welcome to Better Pokedex Entries!</h1>
+        <p>We know the Pokedex in the games can be a little... lackluster. Practice making some!</p>
+        <hr className="my-2" />
+        <p>Start by choosing a pokemon!</p>
+        <p><Link to="/pokemon"><Button color="primary">Pokemon List</Button></Link></p>
+      </Jumbotron>
 
-      
-        <Jumbotron>
-          <h1>Welcome to Better Pokedex Entries!</h1>
-          <p>We know the Pokedex in the games can be a little... lackluster. Practice making some!</p>
-          <hr className="my-2" />
-          <p>Start by choosing a pokemon!</p>
-          <p><Link to="/pokemon"><Button color="primary">Pokemon List</Button></Link></p>
-        </Jumbotron>
+    {randomPokes.map(pokemon => <PokemonCards key={pokemon.id} pokemon={pokemon} />)}
 
-      {randomPokes.map(pokemon => <PokemonCards key={pokemon.id} pokemon={pokemon} />)}
-      
     </div>
   )
 }
