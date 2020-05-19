a612cfaf49d6a5cce0d8fa4616754d99d74d04a6
Shujaat Azim <shujaat.azim@gmail.com>
2020-05-18 21:04:03 -0400

    Removed service worker, nothing else. Wanted to be on fresh starting point.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Pokemon from './Components/Pokemon'
 import PokemonProfile from './Components/PokemonProfile'
@@ -12,6 +11,6 @@ ReactDOM.render(
     <Route exact path="/" component={App} />
     <Route exact path="/pokemon" component={Pokemon} />
     <Route exact path="/pokemon/:name" render={(props) => <PokemonProfile {...props} />} />
-  </Router>, document.getElementById('root'))
+  </Router>, document.getElementById('root')
+)
 
-serviceWorker.unregister()
